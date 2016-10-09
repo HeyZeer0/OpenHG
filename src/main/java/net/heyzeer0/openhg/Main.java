54e02f3aec98342c08d7628b0f0416f013a726b9
Hey Zeer0 <Hey Zeer0>
2016-10-09 12:43:48 -0300

    Initial commit

---
+++ src/main/java/net/heyzeer0/openhg/Main.java
@@ -0,0 +1,20 @@
+package net.heyzeer0.openhg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by HeyZeer0 on 09/10/2016.
+ */
+public class Main extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+
+
+}
