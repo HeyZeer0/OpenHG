c978b7ce636eaa8d865032da7522449f0622189f
HeyZeer0 <heyzeer0@yahoo.com.br>
2016-10-09 17:11:53 -0300

    Fixed damage on start and invencibility.

--- src/main/java/net/heyzeer0/openhg/eventos/EntityEvent.java
+++ src/main/java/net/heyzeer0/openhg/eventos/EntityEvent.java
@@ -2,8 +2,11 @@ package net.heyzeer0.openhg.eventos;
 
 import net.heyzeer0.openhg.Main;
 import net.heyzeer0.openhg.enums.Estagio;
+import net.heyzeer0.openhg.manager.DamageManager;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 
@@ -23,6 +26,19 @@ public class EntityEvent implements Listener {
     public void lostHunger_pregame(FoodLevelChangeEvent e) {
         if(Main.estagio_atual == Estagio.AGUARDANDO || Main.estagio_atual == Estagio.PREJOGO) {
             e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void damage_pregame(EntityDamageEvent e) {
+        if(Main.estagio_atual == Estagio.AGUARDANDO || Main.estagio_atual == Estagio.PREJOGO) {
+            e.setCancelled(true);
+            return;
+        }
+        if(!DamageManager.isActive()) {
+            if(e.getEntity() instanceof Player) {
+                e.setCancelled(true);
+            }
         }
     }
 
